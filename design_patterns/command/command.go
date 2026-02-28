9f934fcaec01a321be77df2e149beb03c96abb3f
u-toru42 <golt7040@gmail.com>
2026-02-28 11:26:10 +0900

    Command Pattern

---
+++ design_patterns/command/command.go
@@ -0,0 +1,45 @@
+package command
+
+type Command interface {
+	Execute() string
+
+}
+
+type Light struct{}
+
+func (l *Light) On() string {
+	return "Light is On"
+}
+
+func (l *Light) Off() string {
+	return "Light is Off"
+}
+
+type LightOnCommand struct {
+	light *Light
+}
+
+func NewLightOnCommand(light *Light) *LightOnCommand {
+	return &LightOnCommand {
+		light: light,
+	}
+}
+
+func (c *LightOnCommand) Execute() string {
+	return c.light.On()
+}
+
+type SimpleRemoteControl struct {
+	slot Command
+}
+
+func (r *SimpleRemoteControl) SetCommand(cmd Command) {
+	r.slot = cmd
+}
+
+func (r *SimpleRemoteControl) ButtonWasPressed() string {
+	if r.slot != nil {
+		return r.slot.Execute()
+	}
+	return "No command assigned"
+}

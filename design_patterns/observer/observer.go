7b0a68e0b148cc20f99dd0c5c9d105ba7af3c005
u-toru42 <golt7040@gmail.com>
2026-03-05 21:29:54 +0900

    observer pattern

---
+++ design_patterns/observer/observer.go
@@ -0,0 +1,68 @@
+package main
+
+import "fmt"
+
+type Observer interface {
+	Update(itemName string)
+}
+
+type Subject interface {
+	Register(observer Observer)
+	Deregister(observer Observer)
+	NotifyAll()
+}
+
+type Item struct {
+	observerList []Observer
+	name         string
+	inStock      bool
+}
+
+func NewItem(name string) *Item {
+	return &Item{
+		name: name,
+	}
+}
+
+func (i *Item) Register(o Observer) {
+	i.observerList = append(i.observerList, o)
+}
+
+func (i *Item) Deregister(o Observer) {
+
+}
+
+func (i *Item) NotifyAll() {
+	for _, observer := range i.observerList {
+		observer.Update(i.name)
+	}
+}
+
+func (i *Item) UpdateAvailability() {
+	fmt.Printf("System: %s が入荷しましたよ!", i.name)
+	i.inStock = true
+	i.NotifyAll()
+}
+
+type Customer struct {
+	id           string
+	ReceivedMsgs []string
+}
+
+func (c *Customer) Update(itemName string) {
+	msg := fmt.Sprintf("お客様 %s: %s が入荷しましたよ!", c.id, itemName)
+	c.ReceivedMsgs = append(c.ReceivedMsgs, msg)
+	fmt.Println(msg)
+}
+
+func main() {
+	nintendoSwitch := NewItem("Nintendo Switch")
+
+	customer1 := &Customer{id: "Alice"}
+	customer2 := &Customer{id: "Bob"}
+
+	nintendoSwitch.Register(customer1)
+	nintendoSwitch.Register(customer2)
+
+	nintendoSwitch.UpdateAvailability()
+}

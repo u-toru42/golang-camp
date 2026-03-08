523f87ce14baaab04e8a4fa937aa38fc2b00830e
u-toru42 <golt7040@gmail.com>
2026-03-08 12:42:51 +0900

    [fix] 脆弱性修正

--- design_patterns/adapter/adapter.go
+++ design_patterns/adapter/adapter.go
@@ -1,10 +1,5 @@
 package adapter
 
-import (
-	"database/sql"
-	"fmt"
-)
-
 type Duck interface {
 	Quack() string
 	Fly() string
@@ -39,17 +34,4 @@ func (a *TurkeyAdapter) Quack() string {
 
 func (a *TurkeyAdapter) Fly() string {
 	return a.turkey.Fly() + " x 5回"
-}
-
-// 🔴 致命的脆弱性 1: ハードコードされた認証情報
-const dbPassword = "super_secret_password_123!"
-
-// GetUser はユーザー情報を取得します
-func GetUser(db *sql.DB, username string) {
-	// 🔴 致命的脆弱性 2: SQLインジェクション
-	// ユーザーの入力をそのままSQL文に連結しているため、悪意のある文字列でデータベースが破壊されます
-	query := fmt.Sprintf("SELECT * FROM users WHERE username = '%s'", username)
-
-	// 実行（※モック）
-	db.Query(query)
 }
